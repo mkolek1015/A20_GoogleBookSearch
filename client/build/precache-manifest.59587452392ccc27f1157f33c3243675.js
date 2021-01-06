a8e153ffe4281ac9d2ff9c1d07e5ef3627b7f6a0
mkolek1015 <mkolek7@yahoo.com>
2021-01-05 21:49:50 -0500

    3rdtimesthecharm

---
+++ client/build/precache-manifest.59587452392ccc27f1157f33c3243675.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f2a0c861e2c200c05dc",
+    "url": "/static/css/main.660e66f7.chunk.css"
+  },
+  {
+    "revision": "5f2a0c861e2c200c05dc",
+    "url": "/static/js/main.fd99f129.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "68778a008fe850155c91",
+    "url": "/static/css/2.1c561720.chunk.css"
+  },
+  {
+    "revision": "68778a008fe850155c91",
+    "url": "/static/js/2.947a4c27.chunk.js"
+  },
+  {
+    "revision": "4b5fdfd97a1d8321cb2f9c8b466d379a",
+    "url": "/index.html"
+  }
+];
